341293e86b4f68c060b264898e8a8239f40572e1
Ewout van Mansom <ewout@freedom.nl>
2018-05-17 12:03:42 +0200

    Use libuv instead of pthread

--- src/log.c
+++ src/log.c
@@ -20,102 +20,102 @@
  * IN THE SOFTWARE.
  */
 
+#include <pthread.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <string.h>
 #include <time.h>
-#include <pthread.h>
+#include <uv.h>
 
 #include "log.h"
 
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+uv_mutex_t m;
 
 static struct {
-  FILE *fp;
-  int level;
-  int quiet;
+    FILE* fp;
+    int level;
+    int quiet;
 } L;
 
-
-static const char *level_names[] = {
-  "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
+static const char* level_names[] = {
+    "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
 };
 
 #ifdef LOG_USE_COLOR
-static const char *level_colors[] = {
-  "\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
+static const char* level_colors[] = {
+    "\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
 };
 #endif
 
-
-static void lock(void) {
-    pthread_mutex_lock(&m);
+static void lock(void)
+{
+    uv_mutex_lock(&m);
 }
 
-
-static void unlock(void) {
-    pthread_mutex_unlock(&m);
+static void unlock(void)
+{
+    uv_mutex_unlock(&m);
 }
 
-
-void log_set_fp(FILE *fp) {
-  L.fp = fp;
+void log_set_fp(FILE* fp)
+{
+    L.fp = fp;
 }
 
-
-void log_set_level(int level) {
-  L.level = level;
+void log_set_level(int level)
+{
+    L.level = level;
 }
 
-
-void log_set_quiet(int enable) {
-  L.quiet = enable ? 1 : 0;
+void log_set_quiet(int enable)
+{
+    L.quiet = enable ? 1 : 0;
 }
 
+void log_log(int level, const char* file, int line, const char* fmt, ...)
+{
+    if (level < L.level) {
+	return;
+    }
 
-void log_log(int level, const char *file, int line, const char *fmt, ...) {
-  if (level < L.level) {
-    return;
-  }
+    /* Acquire lock */
+    lock();
 
-  /* Acquire lock */
-  lock();
+    /* Get current time */
+    time_t t = time(NULL);
+    struct tm* lt = localtime(&t);
 
-  /* Get current time */
-  time_t t = time(NULL);
-  struct tm *lt = localtime(&t);
-
-  /* Log to stderr */
-  if (!L.quiet) {
-    va_list args;
-    char buf[16];
-    buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
+    /* Log to stderr */
+    if (!L.quiet) {
+	va_list args;
+	char buf[16];
+	buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
 #ifdef LOG_USE_COLOR
-    fprintf(
-      stderr, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
-      buf, level_colors[level], level_names[level], file, line);
+	fprintf(
+	    stderr, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
+	    buf, level_colors[level], level_names[level], file, line);
 #else
-    fprintf(stderr, "%s %-5s %s:%d: ", buf, level_names[level], file, line);
+	fprintf(stderr, "%s %-5s %s:%d: ", buf, level_names[level], file, line);
 #endif
-    va_start(args, fmt);
-    vfprintf(stderr, fmt, args);
-    va_end(args);
-    fprintf(stderr, "\n");
-  }
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+	fprintf(stderr, "\n");
+    }
 
-  /* Log to file */
-  if (L.fp) {
-    va_list args;
-    char buf[32];
-    buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", lt)] = '\0';
-    fprintf(L.fp, "%s %-5s %s:%d: ", buf, level_names[level], file, line);
-    va_start(args, fmt);
-    vfprintf(L.fp, fmt, args);
-    va_end(args);
-    fprintf(L.fp, "\n");
-  }
+    /* Log to file */
+    if (L.fp) {
+	va_list args;
+	char buf[32];
+	buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", lt)] = '\0';
+	fprintf(L.fp, "%s %-5s %s:%d: ", buf, level_names[level], file, line);
+	va_start(args, fmt);
+	vfprintf(L.fp, fmt, args);
+	va_end(args);
+	fprintf(L.fp, "\n");
+    }
 
-  /* Release lock */
-  unlock();
+    /* Release lock */
+    unlock();
 }
